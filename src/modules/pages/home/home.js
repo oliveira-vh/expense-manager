7567b56325d77ce6fa2e17184533a2e23cd60d2f
Victor Oliveira <oliveira.wn@gmail.com>
2023-10-25 20:40:48 -0300

    mock api

--- src/modules/pages/home/home.js
+++ src/modules/pages/home/home.js
@@ -1,3 +1,35 @@
 import { LightningElement } from 'lwc';
+const SERVER_URL = 'http://localhost:3004'
+export default class Home extends LightningElement{
+    async connectedCallback(){
+      const expenses = await this.getExpenses()
+      console.log("expenses", expenses)
+    }
 
-export default class Home extends LightningElement {}
+    //Method to get Expenses data
+    async getExpenses(){
+        const url = `${SERVER_URL}/expenses`
+        return await this.makeApiRequest(url)
+    }
+
+    //Generic API Method
+    async makeApiRequest(url, method = 'GET', data=null){
+        try{
+            const requestOptions = {
+                method,
+                headers:{
+                    'Content-Type':'application/json'
+                },
+                body:data ? JSON.stringify(data):null
+            }
+            const response = await fetch(url, requestOptions)
+            if(!response.ok){
+                throw new Error(response.statusText)
+            }
+            return response.json()
+        }catch(error){
+            console.log("Error Occurred", error)
+        }
+
+    }
+}
